b6b9e26953bd6ce962bf1ff30382a26a44dd1cfc
Andrew <asadad@i.ua>
2020-11-29 18:42:39 +0200

    Добавьте файлы проекта.

---
+++ app/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть вагу");
+            string veigh = Console.ReadLine();
+            int veight = Convert.ToInt32(veigh);
+           
+            Console.WriteLine("Введiть зріст");
+            string heigh = Console.ReadLine();
+            double height = Convert.ToInt32(heigh);
+            if (height > 3) {
+                height = height / 100;
+            }
+            double height_kv = height * height;
+            double IMT = veight /height_kv;
+                  
+
+            Console.WriteLine("Ваш ІМТ:"+IMT);
+        }
+    }
+}
